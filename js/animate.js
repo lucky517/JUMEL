5a81dfc08625106952084a3cdbd47d3220d08886
wyt <2437793199@qq.com>
2021-01-06 21:07:45 +0800

    修改

--- js/animate.js
+++
@@ -1,9 +0,0 @@
-function animate(obj,target) {
-    clearInterval(obj.target);
-    obj.timer=setInterval(function(){       
-        if(obj.offsetLeft>=terget){
-            clearInterval(obj.timer)
-        }
-        obj.style.left=obj.offsetLeft+1+'px';
-    },30);
-}
