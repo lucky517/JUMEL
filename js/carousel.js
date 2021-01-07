963ed5829afaa9f107d7fe2c0283e5ee723a498c
wyt <2437793199@qq.com>
2021-01-07 15:05:12 +0800

    更新

--- js/carousel.js
+++ js/carousel.js
@@ -33,3 +33,69 @@ window.addEventListener('load',function(){
     }
     ol.children[0].className='current';
 })
+
+.box_1{
+    position: relative;
+    width:500px;
+    height:190px;
+    background:red;
+    margin:0 auto;
+   
+}
+.box_1 ul{
+    width:600%;
+    position:absolute;
+    top:0;
+    left:0;
+
+}
+.box_1 ul li{
+    float:left;
+}
+.arrow-1,
+.arrow-2{
+    display: none;
+    position: absolute;
+    top:50%;
+    margin-top:-20px;
+    width:40px;
+    height:40px;
+    background: #cccccc;
+    text-align: center;
+    line-height: 40px;
+    color:#fff;
+    font-family: 'icomoon';
+    font-size:18px;
+    text-decoration: none;
+    margin-left:5px;
+}
+.arrow-2{
+    right:0;
+    margin-right:5px;
+}
+.box_1 img{
+    width:500px;
+    height:190px;
+}
+.box_1 li{
+    list-style: none;
+}
+.circle{
+    position: absolute;
+    bottom:0;
+    left:50px;
+    margin-bottom:50px;
+    margin-left:80px;
+}
+.circle li{
+   width:20px;
+   height:20px;
+   border-radius:50%;
+  list-style:none;
+  border:1px solid #cccccc;
+   float: left;
+   margin-left:10px;
+}
+.current{
+    background:#cccccc;
+}
